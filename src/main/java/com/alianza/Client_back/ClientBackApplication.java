2554f31a0a8e90985231a3a50acd3d83382d55e1
Oscar <oscarballestach@gmail.com>
2025-04-28 21:12:59 -0500

    feat: Initializing Project

---
+++ src/main/java/com/alianza/Client_back/ClientBackApplication.java
@@ -0,0 +1,13 @@
+package com.alianza.Client_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alianza/Client_back/ClientBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alianza.Client_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
